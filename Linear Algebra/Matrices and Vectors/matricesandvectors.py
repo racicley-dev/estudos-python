aab0342bf0a7ecc8c6650106edd3067ccb016ad6
Racicley Costa <racicley@gmail.com>
2022-01-30 00:15:29 -0300

    Segundo commit: Alguma alterações no arquivo matricesandvectors.py

--- Linear Algebra/Matrices and Vectors/matricesandvectors.py
+++ Linear Algebra/Matrices and Vectors/matricesandvectors.py
@@ -20,18 +20,52 @@ def criar_vetor():
 
     return Vector
 
+def mostrar_op_matriz():
+    print(100 * '-')
+    
+    print ('0 - Produto por escalar.\n')
+
+    print ('1 - Produto por matricial.\n')
+    
+    print(100 * '-')
+
+def mostrar_op_vetorial():
+    print(100 * '-')
+    
+    print ('0 - Produto por escalar.\n')
+
+    print ('1 - Soma com outro vetor.\n')
+    
+    print(100 * '-')
+    
+
 def monta():
     tipo = input('Insira M para Matriz ou V para Vetor:\n')
     if tipo == 'M':
         return (criar_matriz(),'M')
     else:
         return (criar_vetor(),'V')
+ 
+def produto_p_escalar(v):
+    d = input('Digite o digito para multiplicar o vetor: ')
+    return int(d) * v[0]
 
 
 def main():
     matriz_vetor = monta()
 
-    print(matriz_vetor[0])
+    if matriz_vetor[1] == 'M':
+        mostrar_op_matriz()
+        op = input('')
+    elif matriz_vetor[1] == 'V':
+        mostrar_op_vetorial()
+        op = input('')
+
+    if int(op) == 0:
+        print(100 * '-')
+        print(matriz_vetor[0])
+        print(100 * '-')
+        print(produto_p_escalar(matriz_vetor))
 
     #vectors
     #v = array([1.,2.,3.])
