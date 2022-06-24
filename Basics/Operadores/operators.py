790dfa7529a452ecbbf7705691c4de4a3ceec9e1
Racicley <racicley@gmail.com>
2022-06-24 19:02:14 -0300

    Adição em operadores

--- Basics/Operadores/operators.py
+++ Basics/Operadores/operators.py
@@ -34,3 +34,60 @@ print('-7 // 4 = ',-7 // 4,'\n')  # integer div., result not the opposite of pre
 
 print('*' * 100)
 print('*' * 100)
+
+## Part 3
+
+print('int(1.75) = ',int(1.75))
+
+print('int(-1.75) = ', int(-1.75))
+
+print('int(\'10110\', base=2) = ', int('10110', base=2))
+
+print('*' * 100)
+print('*' * 100)
+
+print('pow(10, 3) = ', pow(10, 3) )
+
+print('10 ** 3 = ', 10 ** 3)
+
+print('pow(10, -3) = ', pow(10, -3))
+
+print('10 ** -3 = ', 10 ** -3)
+
+print('10 % 3 = ',10 % 3)
+
+print('10 % 4 = ', 10 % 4)
+
+print('*' * 100)
+print('*' * 100)
+
+print('pow(123, 4) = ', pow(123, 4))
+print('pow(123, 4, 100) = ', pow(123, 4, 100))
+print('pow(37, -1, 43) = ',pow(37, -1, 43))
+print('7 * 37 % 43 = ', 7 * 37 % 43)
+
+print('*' * 100)
+print('*' * 100)
+
+#make some numbers more readable, use _ underscore
+n = 1_000_000_000
+
+#Hexdecimal
+hex_n = 0x_4_0_0
+
+print('*' * 100)
+print('*' * 100)
+
+print('int(True) = ', int(True))
+print('int(False) = ', int(False))
+print('bool(1) = ', bool(1))
+print('bool(-42) = ', bool(-42))
+print('bool(0) = ', bool(0))
+print('not True = ', not True)
+print('not False = ', not False)
+print('True and True = ', True and True)
+print('True or True = ', False or True)
+
+print('1 + True = ', 1 + True)
+print('False + 42 = ', False + 42)
+print('7 - True = ',7 - True)
